45be027408ddab2afa7830add2b17e54c7b56327
junyeobKim95 <jy.kim@boosters.kr>
2023-04-17 15:29:25 +0900

    2023-04-17 etl폴더명 변경

--- ubuntu18.04/init.sh
+++ ubuntu18.04/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 userName="stage";
-mkdir -p /home/centos/dev/${userName}/https /home/centos/dev/${userName}/settlement_crawling /home/centos/dev/${userName}/process /home/centos/dev/${userName}/etl
+mkdir -p /home/centos/dev/${userName}/https /home/centos/dev/${userName}/settlement_crawling /home/centos/dev/${userName}/process /home/centos/dev/${userName}/settlement
 mkdir -p /home/centos/dev/${userName}/https/log
 
 echo -e "\n" >> ~/.bashrc
@@ -11,7 +11,7 @@ echo "alias stage_h='cd /home/centos/dev/stage/https'" >> ~/.bashrc
 echo -e "\n" >> ~/.bashrc
 echo "alias stage_p='cd /home/centos/dev/stage/process'" >> ~/.bashrc
 echo -e "\n" >> ~/.bashrc
-echo "alias stage_e='cd /home/centos/dev/stage/etl'" >> ~/.bashrc
+echo "alias stage_s='cd /home/centos/dev/stage/settlement'" >> ~/.bashrc
 echo -e "\n" >> ~/.bashrc
 echo "alias stage_c='cd /home/centos/dev/stage/settlement_crawling'" >> ~/.bashrc
 echo -e "\n" >> ~/.bashrc
